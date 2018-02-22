202c30bc797eca5050a18c6124cb2843645e8cb8
sk-mk2 <ee57042@meiji.ac.jp>
2018-02-22 21:54:02 +0900

    new login test

--- app/hello.js
+++ app/hello.js
@@ -5,6 +5,15 @@ const app = express();
 app.get("/", (req,res) => {
     res.send("hello, world");
 });
+
+app.get("/login", (req,res) => {
+    res.send("login");
+});
+
+app.get("/new", (req,res) => {
+    res.send("new");
+});
+
 const server = app.listen(process.env.PORT||3000, function () {
     const host = server.address().address;
     const port = server.address().port;
